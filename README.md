e6ebee70b7e6d774f3fb0abecf58e045fa4a5937
Tyler Hancock <darklime@live.ca>
2026-01-03 16:04:26 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,56 @@
+# Overstacked
+
+The Overstacked plugin increases the max stack size of several commonly used items to be higher. It also offers a 
+powerful config system that allows you to modify the max stack size of any item in the game!
+
+## Default Changes
+The plugin will make the following changes by default.
+
+- Ores `25` -> `50`
+- Wood Trunks `100` -> `250`
+- Wood Planks `100` -> `250`
+- Fibre `100` -> `250`
+- Soils `100` -> `250`
+- Rocks `100` -> `250`
+- Petals `100` -> `300`
+- Rubble `100` -> `150`
+- Crystal Shards `100` -> `150`
+- Seeds `100` -> `150`
+
+## Configuration
+When you run the game for the first time after installing the mod, a `Darkhax_Overstacked` folder will be generated in 
+the mods folder. Inside this folder you will find a `MaxStackSizes.example.json` file, which is both an example and the
+default changes made by the mod. To make your own changes, copy the file and name it `MaxStackSizes.json`. The file can
+be edited with notepad or any other text editor.
+
+### ItemIds
+The `ItemIds` section of the config is a direct map of item IDs to new stack sizes. You can increase or decrease the 
+stack size, as long as the size is not less than 1.
+
+```json
+{
+  "ItemIds": {
+    "Rock_Stone": 999,
+    "Soil_Dirt": 5
+  }
+}
+```
+
+If you don't want the mod to modify an item, just remove it from the map. All map entries are optional and up to you.
+
+### Regex Patterns
+The `Patterns` section allows you to define a regex pattern. Any ID matching the regex will be updated to the new size. 
+For example the pattern `^Furniture_.*_Stool$` will match any item that starts with `Furniture_` and ends with `_Stool`.
+Regex patterns are very powerful because they allow you to make sweeping changes. They can also be dangerous as there is
+a potential for them to match on something you did not account for.
+
+```json
+{
+  "ItemIds": {
+    "Rock_Stone": 999
+  },
+  "Patterns": {
+    "^Furniture_.*_Stool$": 200
+  }
+}
+```
---
+++ src/main/java/net/darkhax/overstacked/Overstacked.java
@@ -0,0 +1,54 @@
+package net.darkhax.overstacked;
+
+import com.hypixel.hytale.assetstore.event.LoadedAssetsEvent;
+import com.hypixel.hytale.assetstore.map.DefaultAssetMap;
+import com.hypixel.hytale.logger.HytaleLogger;
+import com.hypixel.hytale.server.core.asset.type.item.config.Item;
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import com.hypixel.hytale.server.core.util.Config;
+
+import javax.annotation.Nonnull;
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+public class Overstacked extends JavaPlugin {
+
+    private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
+    private final Config<MaxStackSizeConfig> cfg;
+
+    public Overstacked(@Nonnull JavaPluginInit init) {
+        super(init);
+        this.cfg = this.withConfig("MaxStackSizes", MaxStackSizeConfig.CODEC);
+    }
+
+    protected void setup() {
+        this.getEventRegistry().register(LoadedAssetsEvent.class, Item.class, this::onItemAssetLoad);
+    }
+
+    private void onItemAssetLoad(@Nonnull LoadedAssetsEvent<String, Item, DefaultAssetMap<String, Item>> event) {
+        try {
+            final Map<String, Item> assets = event.getLoadedAssets();
+            final Field maxStackSize = Item.class.getDeclaredField("maxStack");
+            maxStackSize.setAccessible(true);
+            for (Map.Entry<String, Integer> entry : cfg.get().itemIds.entrySet()) {
+                final Item item = assets.get(entry.getKey());
+                if (item != null) {
+                    maxStackSize.set(item, entry.getValue());
+                }
+            }
+            for (Map.Entry<String, Integer> entry : cfg.get().patterns.entrySet()) {
+                final Pattern pattern = Pattern.compile(entry.getKey());
+                for (String id : assets.keySet()) {
+                    if (pattern.matcher(id).matches()) {
+                        maxStackSize.set(assets.get(id), entry.getValue());
+                    }
+                }
+            }
+        }
+        catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
